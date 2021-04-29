a682a08ed752c04ab6b7e57865c76620ec76d7cc
isaquelopes047 <isaque.lopes047@gmail.com>
2021-04-29 19:52:39 -0300

    atualização

--- src/Components/infoheader.jsx
+++ src/Components/infoheader.jsx
@@ -1,22 +1,18 @@
 import React from 'react'
 
-
 function open(){
     document.querySelector(".modal_info");
     document.querySelector(".modal_info").style.display = "block";
-    document.querySelector(".Container_body").style.opacity = "0.5";
 }
 function close(){
     document.querySelector(".modal_info");
     document.querySelector(".modal_info").style.display = "none";
-    document.querySelector(".Container_body").style.opacity = "";
 }
 
 class Infodiaria extends React.Component{
 
     render(){
         return(
-            
             <div className="Container_indo_header"> 
 
                 <button onClick={open}><i class="bi bi-chat-right-text-fill"> </i></button>
@@ -24,6 +20,8 @@ class Infodiaria extends React.Component{
                 <div className="modal_info">
                     <div className="slidto" onClick={close}><i class="bi bi-x-circle-fill"> </i></div>
                 <div className="container_info_diaria">
+
+                    <h4>Tabela de Diária </h4>
                     <p> <i class="bi bi-tags-fill"></i> 65,60 - 1 Diária</p>
                     <p> <i class="bi bi-tags-fill"></i> 131,20 - 2 Diárias</p>
                     <p> <i class="bi bi-tags-fill"></i> 196,80 - 3 Diárias</p>
@@ -34,6 +32,16 @@ class Infodiaria extends React.Component{
                     <p> <i class="bi bi-tags-fill"></i> 524,80 - 8 Diárias</p>
                     <p> <i class="bi bi-tags-fill"></i> 590,40 - 9 Diárias</p>
                     <p> <i class="bi bi-tags-fill"></i> 656,00 - 10 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 721,60 - 11 Diária</p>
+                    <p> <i class="bi bi-tags-fill"></i> 787,20 - 12 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 852,80 - 13 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 918,40 - 14 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 984,00 - 15 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 1.049,60 - 16 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 1.115,20 - 17 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 1.180,80 - 18 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 1.246,40 - 19 Diárias</p>
+                    <p> <i class="bi bi-tags-fill"></i> 1.312,00 - 20 Diárias</p>
                 </div>
             </div>
         </div>
